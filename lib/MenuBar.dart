c807ad39762e5e1c0a2a116c91d3bf4dba4112e7
seungyeon kim <rlatmddus159@naver.com>
2023-05-22 12:57:22 +0900

    snackbar

--- lib/MenuBar.dart
+++ lib/MenuBar.dart
@@ -27,13 +27,88 @@ class MyPage extends StatelessWidget {
         title: Text('icon menu'),
         centerTitle: true,
         elevation: 0.0,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            print('menu button clicked');
-          },
-        ),
+        // leading: IconButton(
+        //   icon: Icon(Icons.menu),
+        //   onPressed: () {
+        //     print('menu button clicked');
+        //   },
+        // ),
+        actions: [
+          IconButton(onPressed: () {
+          }, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {
+          }, icon: Icon(Icons.shopping_cart))
+        ],
       ),
+
+      body: Center(
+        child: ElevatedButton(onPressed: () {  },
+          child: Text('Show me', style: TextStyle(color: Colors.white),),
+          style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.orange)),
+        )
+      ),
+
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/sample1.png'),
+                backgroundColor: Colors.red[400],
+              ),
+              otherAccountsPictures: [
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/sample2.png'),
+                backgroundColor: Colors.red[400],
+              ),
+              CircleAvatar(
+                  backgroundImage: AssetImage('assets/sample2.png'),
+                  backgroundColor: Colors.red[400],
+                )
+              ],
+              accountName: Text('BBanto'),
+              accountEmail: Text('rlatmddus159@naver.com'),
+              onDetailsPressed: () {
+              },
+              decoration: BoxDecoration(
+                color: Colors.red[400],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40),
+                  bottomRight: Radius.circular(40)
+                )
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home,
+              color: Colors.grey[850]),
+              title: Text('Home'),
+              onTap: () {
+                print('clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings,
+                  color: Colors.grey[850]),
+              title: Text('Setting'),
+              onTap: () {
+                print('clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer,
+                  color: Colors.grey[850]),
+              title: Text('Q&A'),
+              onTap: () {
+                print('clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+          ],
+        ),
+      ) ,
 
 
     );
